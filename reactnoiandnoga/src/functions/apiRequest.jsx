be9059e9fc25bc3817a1f076deffb709ed85fbe3
noi <noieitan@hilma.tech>
2024-11-12 15:59:59 +0200

    post noi

--- reactnoiandnoga/src/functions/apiRequest.jsx
+++ reactnoiandnoga/src/functions/apiRequest.jsx
@@ -4,9 +4,9 @@ const apiRequest = async (url = "", optionObj = null, errMsg = null) => {
     if (!response.ok) throw Error("error occoured during request");
   } catch (err) {
     errMsg = err.message;
-    return errMsg;
   } finally {
     return errMsg;
   }
 };
+
 export default apiRequest;
---
+++ reactnoiandnoga/src/functions/postRequest.jsx
@@ -0,0 +1,45 @@
+// import { apiRequest } from "apiRequest";
+
+// const addTodos = async (userId) => {}
+
+// const TodoId = users.length ? users[users.length - 1].id + 1 : 1;
+// const newTodo = { id, checked: false,  };
+// const listUsers = [...users, newUser];
+// setItems(listUsers);
+
+// const postOptions = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": `${API_URL}/users`,
+//   },
+//   body: JSON.stringify(newUser),
+// };
+// const result = await apiRequest(`${API_URL}/users`, postOptions);
+
+// const addPosts = async (postId) => {}
+
+// const addAlbums = async (albumId) => {}
+
+// const addComments = async ({postId}) => {
+
+// }
+
+export const addusers = async (obj) => {
+  const id = Math.random() * 5000;
+  const newUser = { ...obj, id: id };
+
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": `${API_URL}/users`,
+    },
+    body: JSON.stringify(newUser),
+  };
+  const result = await fetch(`${API_URL}/users`, postOptions);
+};
+
+// export addTodos;
+// export addPosts;
+// export addAlbums;
+// export addComments;
+// export addusers;
