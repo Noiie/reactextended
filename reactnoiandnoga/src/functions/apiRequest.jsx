d071cca6dde386a14928c6fc10923c8ed72ca559
shemesh-noga <nogashemesh@gmail.com>
2024-11-12 15:59:23 +0200

    get

--- reactnoiandnoga/src/functions/apiRequest.jsx
+++ reactnoiandnoga/src/functions/apiRequest.jsx
@@ -2,9 +2,9 @@ const apiRequest = async (url = "", optionObj = null, errMsg = null) => {
   try {
     const response = await fetch(url, optionObj);
     if (!response.ok) throw Error("error occoured during request");
+    return response;
   } catch (err) {
     errMsg = err.message;
-    return errMsg;
   } finally {
     return errMsg;
   }
---
+++ reactnoiandnoga/src/functions/getRequest.jsx
@@ -0,0 +1,87 @@
+import apiRequest from "./apiRequest";
+
+const Api_Url = "http://localhost:3500/";
+
+export async function getPosts(userId) {
+  try {
+    const request = await fetch(`http://localhost:3000/posts`);
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
+
+export async function getComments(postId) {
+  try {
+    const request = await fetch(
+      `http://localhost:3000/comments?postId=${postId}`
+    );
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
+
+export async function getTodos(userId) {
+  try {
+    const request = await fetch(`http://localhost:3000/todos?userId=${userId}`);
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
+
+export async function getAlbums(userId) {
+  try {
+    const request = await fetch(
+      `http://localhost:3000/albums?userId=${userId}`
+    );
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
+
+export async function getPhotos(albumId) {
+  try {
+    const request = await fetch(
+      `http://localhost:3000/photos?albumId=${albumId}`
+    );
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
+
+export async function getUser(userId) {
+  try {
+    const request = await fetch(`http://localhost:3000/users?id=${userId}`);
+    if (!request.ok) throw Error("Did not get expected data");
+    const requestJson = await request.json();
+    console.log(requestJson);
+  } catch (err) {
+    console.log(err);
+    return err.message;
+  } finally {
+  }
+}
