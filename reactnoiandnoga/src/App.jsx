1f27e40c848ab2f30fa09e63ab6119598b7a5847
noi <noieitan@hilma.tech>
2024-11-12 16:03:41 +0200

    jjj

--- reactnoiandnoga/src/App.jsx
+++ reactnoiandnoga/src/App.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import { addusers } from "./functions/postRequest";
 import "./App.css";
 
 function App() {
   const API_URL = "http://localhost:3000/";
-  const [users, setusers] = useState([]);
-  const [newUser, setNewUser] = useState("");
+
   useEffect(() => addusers({}), []);
 
   return (
